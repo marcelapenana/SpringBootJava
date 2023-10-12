c6ad5707f2504d441335371cf8f85cecce00e08b
Marcela Najera <marcelanajera1717@gmail.com>
2023-10-12 17:04:27 -0600

    springboot-web proyect firs spring boot

--- Semana2/EjercicioClase/src/main/java/org/aguzman/hibernateapp/Main.java
+++
@@ -1,9 +0,0 @@
-package org.aguzman.hibernateapp;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    //Estudiantes pertenecen a un grupo
-    //Estudiantes tienen notas
-}
