9ca5afa3b4f67c35f7020457f5b0f4a34de9a05b
Marcela Najera <marcelanajera1717@gmail.com>
2023-10-27 13:27:24 -0600

    add

---
+++ Semana4/spring-boot-farmacia/src/main/java/springbootfarmacia/SpringBootFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package springbootfarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFarmaciaApplication.class, args);
+	}
+
+}
