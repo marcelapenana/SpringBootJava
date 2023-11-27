c3f6f98da4548ad5aef461d9bf47cf78649262af
Marcela Najera <marcelanajera1717@gmail.com>
2023-11-27 14:24:34 -0600

    update

--- Semana7/universidad-backend-GuzMel/src/main/java/com/universidadbackend/UniversidadBackendApplication.java
+++ Semana7/universidad-backend-GuzMel/src/main/java/com/universidadbackend/UniversidadBackendApplication.java
@@ -3,7 +3,7 @@ package com.universidadbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class UniversidadBackendApplication {
 
 
